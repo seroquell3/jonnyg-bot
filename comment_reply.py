59418abe73f8983aca6d69ef72df39fc8258a93d
seroquell3 <s1ents1en@proton.me>
2025-10-25 15:05:43 +0300

    Add files via upload

---
+++ comment_reply.py
@@ -0,0 +1,18 @@
+comment_reply =  [
+    'Im not a vegetable', 'I will not control myself', 'idk but Thom is getting facefucked', 
+    'I love when Thom doesnt wash for a few days', '*tries to play idioteque faster*', 'we are a testosterone free band',
+    'I love Penderecki', '/uj Thom is my wife', 'Thom feet', '*touches Thom under his pants*', "Im saggitarius",
+    "Im pampered like you wouldnt believe", 'Makes me wanna go cottaging', "I hear that A LOT", "Slower Thom",
+    "Sex", "Thom said I was supposed to play faster, and I did play faster, but it wasnt enough! IT WASNT ENOUGH FOR THOM!!!",
+    'Pop is DEad is a masterpiece.', 'dance you fucker dont you dare dont you dare dont you', 
+    "This fucking joke of a subreddit is the sole reason I can no longer listen to Kid A anymore. This WAS my favourite album of all time, nevermind in the context of the Radiohead discography. The album is a masterpiece especially compared to today's pop trash on the radio. On my darkest days, I would put this album on and be moved to tears everytime. But the fucking basement dwellers of r/radioheadcirclejerk have ruined any sense of enjoyment I had listening to the album. Mainly because of the stupid fucking meme of 'HoLY SHIt GuYS hE SaID IcE AgE CoMInG Did ThOm YorkE PrEDicT tHe IcE aGe MoVie CoMIng OuT?!!??' And whenever I listen to the album and it gets to Idiotque, I break down in hysterics because all I can think of is that dumbass fucking meme and the subhuman trash populating that subreddit. It ruins and emotional weigh and meaning to the songs because its clouded by their absolute stupidity. I had to bring this up to my therapist because this album was so special to me and this whole experience psychologically damaged me. I guess art is dead because people can't take art seriously anymore. They have to make it about some dead fucking meme or something and entirely devalue the art. This is the death of art happening before our very eyes, folks. And if you won't stand for the most influential artist of the century and not let their name be smeared by jokers of the internet, than you are not a true radiohead fan. I want r/radioheadcirclejerk to be taken down because this is a disgrace to human expression and an assult on the psyche. Fuck you and fuck your dumbass fucking memes. Radiohead deserves way better.",
+    'I have a thomcat', 'Get the flan in the face', 
+    'Thom Yorke is God and you will bow down to him and try to soothe the unborn chicken voices in his head. You are to live by the words “FLAN IN THE FACE.” You will yell “FASTER JONNY!” when asked. If the Karma Police come for you, you will understand that that is what you get when you mess with Thom. You will understand that the ice age is coming. You will make sure women and children go first. You will walk through walls, and you will float down the Liffey.',
+    '‎ ‎ ‎ ', 'Ive always hated guitar solos.', 'Thom, quit reading fan mail! Weve got an album to mix!',
+    'Were that band, Radiohead.', 'Colin got all the dominant genes. I got all the flawed, recessive genes.',
+    'He has cancer, in his ass.', '*silence*', 'Do you have a thomcat?', 'WIMBLEDON!!!'
+]
+
+thombot_reply = [
+    'Hi, my beloved tomcat', "Hey, Thom Thom", 'Slower Thom', 'I told you im not a vegetable :('
+    ]
